e3fe856d09f8d54ae7e4b4200252964ea86fa13a
Exaucé Benjamin Kyalondawa <exaucebenjamink@gmail.com>
2024-08-07 08:40:59 +0100

    server created

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (req, res, next) => {
+  try {
+    console.log("BIENVCENU");
+    return res
+      .status(200)
+      .send("BIENVENU DANS NOTRE APPLOCATION D'ENREGISTREMENT DES CHAISES");
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({error: 'ERRUR DE SERVEUR'})
+  }
+});
+
+app.listen(port, () => {
+  console.log(`My server is running on http://localhost:${port}`);
+});
